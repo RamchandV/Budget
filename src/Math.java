f5e0f6f5cb7e2d884af1a734ba55776a3c306e56
Dodongos <Dodongos@Dodongos-PC>
2017-01-04 15:15:26 -0500

    Added Bills, Payments, Wishlist and Groceries with totals.

--- src/Math.java
+++ src/Math.java
@@ -1,11 +1,42 @@
+import java.io.*;
 
 public class Math {
-	//TODO Calculate gross to post-tax pay
-	//TODO Calculate monthly gross pay
-	//TODO Calculate weekly gross pay
-	//TODO Calculate monthly post-tax pay
-	//TODO Calculate weekly post-tax pay
-	//TODO Calculate total montly bills
-	//TODO Calculate total "required outgoing"
-	//TODO Calculate "spending money"
+	public String totalAmount(String toTotal, File profile) throws IOException {
+		String line;
+		String[] strArry;
+		Double total = 0.0;
+		BufferedReader reader = new BufferedReader(new FileReader(profile));
+		while ((line = reader.readLine()) != null) {
+			if(line.startsWith(toTotal)) {
+				strArry = line.split(":");
+				total += Double.parseDouble(strArry[2]);	
+			}
+		}
+		reader.close();
+		return total.toString();
+	}
+	
+	public String taxes(){
+		return "Pass";
+	}
+	
+	public String spendingMoney(){
+		return "Pass";
+	}
+	
+	public String grossToTaxed(){
+		return "Pass";
+	}
+	
+	public String yearlyGross(){
+		return "Pass";
+	}
+	
+	public String monthlyGross(){
+		return "Pass";
+	}
+	
+	public String weeklyGross(){
+		return "Pass";
+	}
 }
--- src/Profile.java
+++ src/Profile.java
@@ -28,25 +28,84 @@ class Profile {
 	}
 	
 	public static void loadProfileInfo(File profile) throws IOException {
+		Math math = new Math();
 		BufferedReader reader = new BufferedReader(new FileReader(profile));
 		String line;
 		while ((line = reader.readLine()) != null){
 			System.out.println(line);
 		}
+		System.out.println("Your monthly bills total to: " + math.totalAmount("monthlyBill:", profile));
+		System.out.println("Your required payments total to: " + math.totalAmount("requiredPayment:", profile));
+		System.out.println("Your wish list totals to: " + math.totalAmount("wishlistItem:", profile));
+		System.out.println("Your purchased groceries total to: " + math.totalAmount("groceryPurchase:", profile));
 		reader.close();
 	}
 	
 	public static void getProfileInfo(File profile, Scanner keyboard) throws IOException {
 		PrintWriter newProfile = new PrintWriter(profile);
-		System.out.println("Please provide your current yearly salary:");
+		System.out.print("Please provide your current yearly salary -");
 		String yearlySalary = keyboard.nextLine();
 		newProfile.println("yearlySalary:" + yearlySalary);
-		System.out.println("Please provide your current montly bill total:");
-		String montlyBillTotal = keyboard.nextLine();
-		newProfile.println("montlyBillTotal:" + montlyBillTotal);
-		System.out.println("Please provide your current total required outgoing payments:");
-		String outgoingPayments = keyboard.nextLine();
-		newProfile.println("outgoingPayments:" + outgoingPayments);
+		enterMonthlyBills(newProfile, keyboard);
+		enterRequiredPayments(newProfile, keyboard);
+		enterWishlistItems(newProfile, keyboard);
+		enterGroceries(newProfile, keyboard);
 		newProfile.close();
+	}
+	
+	public static void enterMonthlyBills(PrintWriter profile, Scanner keyboard) {
+		System.out.print("Please provide your monthly bill name and amount separated by a colon(:)");
+		System.out.print("Enter \"Done\" when you're finished - ");
+		String input = keyboard.nextLine();
+		while(true) {
+			System.out.print("Please provide your next bill - ");
+			input = keyboard.nextLine();
+			if(input.equals("Done")) {
+				break;
+			}
+			profile.println("monthlyBill:" + input);
+		}
+	}
+	
+	public static void enterRequiredPayments(PrintWriter profile, Scanner keyboard) {
+		System.out.print("Please provide your payment name and amount separated by a colon(:)");
+		System.out.print("Enter \"Done\" when you're finished - ");
+		String input = keyboard.nextLine();
+		while(true) {
+			System.out.print("Please provide next payment - ");
+			input = keyboard.nextLine();
+			if(input.equals("Done")) {
+				break;
+			}
+			profile.println("requiredPayment:" + input);
+		}
+	}
+	
+	public static void enterWishlistItems(PrintWriter profile, Scanner keyboard) {
+		System.out.print("Please provide your wishlist item name, amount and link separated by a colon(:)");
+		System.out.print("Enter \"Done\" when you're finished - ");
+		String input = keyboard.nextLine();
+		while(true) {
+			System.out.print("Please provide next item - ");
+			input = keyboard.nextLine();
+			if(input.equals("Done")) {
+				break;
+			}
+			profile.println("wishlistItem:" + input);
+		}
+	}
+	
+	public static void enterGroceries(PrintWriter profile, Scanner keyboard) {
+		System.out.print("Please provide your grocery purchase date and amount separated by a colon(:)");
+		System.out.print("Enter \"Done\" when you're finished - ");
+		String input = keyboard.nextLine();
+		while(true) {
+			System.out.print("Please provide next shopping total - ");
+			input = keyboard.nextLine();
+			if(input.equals("Done")) {
+				break;
+			}
+			profile.println("groceryPurchase:" + input);
+		}
 	}
 }
