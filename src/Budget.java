85880ee3284c121716e4387c50ae7d24719d7cca
Dodongos <Dodongos@Dodongos-PC>
2017-01-04 16:26:38 -0500

    Starting super basic GUI and functions

--- src/Budget.java
+++ src/Budget.java
@@ -1,34 +1,50 @@
 import javax.swing.*;
+import java.io.*;
 import java.awt.event.*;
 
 public class Budget extends JFrame {
-	private JPanel panel;
-	private JLabel messageLabel;
-	private JTextField textField;
-	private JButton button; 
-	final int WINDOW_WIDTH = 600;
-	final int WINDOW_HEIGHT = 400;
+	File profile; 
+	private JPanel openProgramWindow;
+	private JLabel welcomeLabel;
+	//private JTextField textField;
+	private JButton newProfileButton, loadProfileButton; 
+	int WINDOW_WIDTH = 450;
+	int WINDOW_HEIGHT = 600;
 	
 	public Budget() {
 		setTitle("Dodongos' Budget Calculator");
 		setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		buildPanel();
-		add(panel);
+		openProgramWindow();
+		add(openProgramWindow);
 		setVisible(true);
 	}
 	
-	public void buildPanel() {
-		messageLabel = new JLabel("Enter your yearly salary");
-		textField = new JTextField(10);
-		button = new JButton("Enter");
-		button.addActionListener(new buttonListener());
-		panel = new JPanel();
-		panel.add(messageLabel);
-		panel.add(textField);
-		panel.add(button);
+	public void openProgramWindow() {
+		welcomeLabel = new JLabel("Welcome to Dodongos Personal Budget Calculator!");
+		newProfileButton = new JButton("New Profile");
+		loadProfileButton = new JButton("Load Profile");
+		newProfileButton.addActionListener(new newProfileListener());
+		loadProfileButton.addActionListener(new loadProfileListener());
+		openProgramWindow = new JPanel();
+		openProgramWindow.add(welcomeLabel);
+		openProgramWindow.add(newProfileButton);
+		openProgramWindow.add(loadProfileButton);
 	}
 	
+	private class newProfileListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			NewWindow newProfile = new NewWindow("newProfile", profile);
+		}
+	}
+	
+	private class loadProfileListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			NewWindow loadProfile = new NewWindow("loadProfile", profile);
+		}
+	}
+	
+	/**
 	private class buttonListener implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
 			String input;
@@ -39,7 +55,8 @@ public class Budget extends JFrame {
 			//insert to text file to store input
 			JOptionPane.showMessageDialog(null, "Your Salary has been stored as $" + salary + "k per year");
 		}
-	}
+	} 
+	**/
 }
 
 
--- src/ProfileFunctions.java
+++ src/ProfileFunctions.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-class Profile {
+class ProfileFunctions {
 	public static void main(String[] args) throws IOException {
 		loadProfile();
 	}
